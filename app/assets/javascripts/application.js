8d8d32381f397de68d8eb484a633a2338d94d3ea
Will Luna <william.luna@tufts.edu>
2016-03-21 22:59:41 -0300

    outlined front page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,4 +18,12 @@
 
 $(document).ready(function(){
   $('.parallax').parallax();
-});`
+});
+
+$(".button-collapse").sideNav();
+
+$(document).ready(function(){
+  $('.collapsible').collapsible({
+    accordion : true // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+  });
+});
